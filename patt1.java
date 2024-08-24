f4f1cd89185bba90c47cb1c0bf56eeea96facc56
Aryan Dangat. <140824379+Aryan2052@users.noreply.github.com>
2024-08-24 10:16:40 +0530

    Update patt1.java

--- patt1.java
+++ patt1.java
@@ -1,1 +1,19 @@
+class Solution {
+    public void pattern1(int n) {
+        for(int row=0; row<n; row++){
+            for(int col=0; col<n; col++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
 
+    }
+    public static void main(String[] args) {
+        int N = 4;
+
+        // Create an instance of the Solution class
+        Solution sol = new Solution();
+
+        sol.pattern1(N);
+    }
+}
